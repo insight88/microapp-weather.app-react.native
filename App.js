e4d79b52032ac4779259c28fa98ae8ebb6ecb818
insight88 <64120339+insight88@users.noreply.github.com>
2020-10-08 15:38:20 +0900

    0.1 expo init project, flex-box in react native

--- App.js
+++ App.js
@@ -5,17 +5,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView}><Text>Hello</Text></View>
+      <View style={styles.blueView}><Text>Hello</Text></View>
     </View>
   );
 }
 
+// * react-native style은 기본적으로 flexDirection : "column"
+// * 화면 크기에 따른 responsive design을 위해서 flex로 구역 크기를 설정하는 것을 추천
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  yellowView: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    textAlign: "center"
+  },
+  blueView: {
+    flex: 3,
+    backgroundColor: 'skyblue',
+    textAlign: "center"
+  }
 });
---
+++ index.js
@@ -0,0 +1,8 @@
+// * npm install -g expo-cli
+
+cli : expo init <project-name>
+// * npx create-react-app과 비슷한 작동
+
+nox 앱 플레이어 설치
+
+android studio 설치
